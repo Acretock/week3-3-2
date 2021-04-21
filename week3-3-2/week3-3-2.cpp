7aa983fa4364ea4240852400f9d3c42e24f6c152
Acretock <georgoncharov@gmail.com>
2021-04-21 16:25:02 +0500

    v1.3

--- week3-3-2/week3-3-2.cpp
+++ week3-3-2/week3-3-2.cpp
@@ -14,32 +14,58 @@ public:
 		//First = name;
 		First.insert({ year,name });
 	}
-	void ChangeLastName(int year, const string& last){
+	void ChangeLastName(int year, const string& last) {
 		//Last = last;
 		Last.insert({ year,last });
 	}
-	string GetFullName(int year) {
-		if ((First.lower_bound(year)->first <= year))/*&& (Last.lower_bound(year)->first <= year))*/ {
-			string tmp = Last.lower_bound(year)->second + " wiht unnown last name";
-			return tmp;
-		}
-		if ((Last.count(year) == 0)&&(First.count(year) != 0)) {
-			string tmp = First.find(year)->second + " with unnown first name";
-			return tmp;
-		}
-		if ((Last.count(year) == 0) && (First.count(year) == 0))
-			return "Incognito";
-		if ((First.lower_bound(year)->first <= year) && (Last.lower_bound(year)->first <= year)) {
-			string tmp = First.find(year)->second + Last.find(year)->second;
-			return tmp;
-		}
 
+
+	int FindFirstYear(int year) {
+		int firstchange = First.lower_bound(0)->first;
+		if (firstchange == year)
+			return year;			// if last == sought-for
+		int first = 0;
+		while ((firstchange < year) && (First.upper_bound(firstchange) != end(First))) {
+			if (First.upper_bound(firstchange)->first > year) {
+				return firstchange;	// return year before sought-for
+			}
+			else
+				firstchange = First.upper_bound(firstchange)->first;
+		}
+		return firstchange;			// if ONLY 1 change has happend
 	}
+	int FindLastYear(int year) {
+		int firstchange = Last.lower_bound(0)->first;
+		if (firstchange == year)
+			return year;			// if last == sought-for
+		int first = 0;
+		while ((firstchange < year) && (Last.upper_bound(firstchange) != end(Last))) {
+			if (Last.upper_bound(firstchange)->first > year) {
+				return firstchange;	// return year before sought-for
+			}
+			else
+				firstchange = Last.upper_bound(firstchange)->first;
+		}
+		return firstchange;			// if ONLY 1 change has happend
+	}
+
+	string GetFullName(int year) {
+		int firstnamechange1 = First.lower_bound(0)->first;
+		int lastnamechange1 = Last.lower_bound(0)->first;
+		if (((firstnamechange1 > year) && (lastnamechange1 > year)))	// if changes happend after year
+			return "Incognito";
+		if ((firstnamechange1 <= year) && (lastnamechange1 > year))		// if first name changed but last wasn't
+			return First.lower_bound(FindFirstYear(year))->second + " with unnown last name";
+		if ((lastnamechange1 <= year) && (firstnamechange1 > year))		// if last name changed but first wasn't
+			return Last.lower_bound(FindLastYear(year))->second + " with unnown last name";
+		if ((lastnamechange1 <= year) && (firstnamechange1 <= year))	// if all was changed
+			return First.lower_bound(FindFirstYear(year))->second + ' ' + Last.lower_bound(FindLastYear(year))->second;
+	}
+
+
 private:
-	map<int,string> First;
-	map<int,string> Last;
-	//string First;
-	//string Last;
+	map<int, string> First;
+	map<int, string> Last;
 };
 
 Person::Person()
@@ -72,3 +98,14 @@ int main()
 	}
 	return 0;
 }
+
+/*
+Incognito
+Polina with unnown last name
+Polina Sergeeva
+Polina Sergeeva
+Appolinaria Sergeeva
+Polina Volkova
+Appolinaria Volkova
+THIS SHOULD BE PRINTED
+*/
